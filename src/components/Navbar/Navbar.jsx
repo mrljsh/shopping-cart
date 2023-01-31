ec3ea1c94d18565da804b41aba77e7fd8c37d8d1
Mrljsh <dragan.mrljes@vsar.edu.rs>
2023-01-31 18:05:47 +0100

    Simple Navbar styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <nav>
+      <h1>Shop</h1>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/shop"}>Shop</Link>
       </div>
-    </div>
+    </nav>
   );
 };
 
