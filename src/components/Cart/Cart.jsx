6cedec24ebe006b2f358083c955683ef61b84b2a
Mrljsh <dragan.mrljes@vsar.edu.rs>
2023-02-02 22:58:15 +0100

    Conditional rendering, it will not render if there is no items in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CartProduct from "./CartProduct";
 import "./Cart.css";
 import fetchProduct from "../../fetchProduct";
 
 const Cart = ({ cart, handleQuantity, handleRemove }) => {
+  const [cartEmpty, setCartEmpty] = useState(true);
+
+  useEffect(() => {
+    //if there is something in cart, cart is not empty, else it's empty
+    cartWithDetails.length > 0 ? setCartEmpty(false) : setCartEmpty(true);
+  }, [cart]);
+
   const cartWithDetails = cart.map((product) => {
     const productData = fetchProduct(product.id);
     const { name, price, image } = productData[0];
@@ -18,21 +25,25 @@ const Cart = ({ cart, handleQuantity, handleRemove }) => {
   return (
     <div className="cart">
       <h1>Your shopping cart</h1>
-      <div className="cart-wrapper">
-        {cartWithDetails.map((product, index) => (
-          <CartProduct
-            index={index}
-            product={product}
-            key={product.id}
-            handleQuantity={handleQuantity}
-            handleRemove={handleRemove}
-          />
-        ))}
-        <p className="total-price">Total price: {totalPrice}$</p>
-        <button className="checkout-btn" disabled="true">
-          Checkout
-        </button>
-      </div>
+      {cartEmpty ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <div className="cart-wrapper">
+          {cartWithDetails.map((product, index) => (
+            <CartProduct
+              index={index}
+              product={product}
+              key={product.id}
+              handleQuantity={handleQuantity}
+              handleRemove={handleRemove}
+            />
+          ))}
+          <p className="total-price">Total price: {totalPrice}$</p>
+          <button className="checkout-btn" disabled="true">
+            Checkout
+          </button>
+        </div>
+      )}
     </div>
   );
 };
