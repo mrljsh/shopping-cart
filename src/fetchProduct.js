a9b399f40826ef62f2f261cf3dcabca031cff8bd
Mrljsh <dragan.mrljes@vsar.edu.rs>
2023-02-03 09:22:39 +0100

    If id is not a number, convert it to number, fetchProduct function

--- src/fetchProduct.js
+++ src/fetchProduct.js
@@ -1,6 +1,9 @@
 import productData from "./productData";
 
 const fetchProduct = (id) => {
+  if (typeof id !== "number") {
+    id = +id;
+  }
   return productData.filter((product) => product.id === id);
 };
 
