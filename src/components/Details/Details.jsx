8d53d512c39531b433079960fd591eb1f6e90745
Mrljsh <dragan.mrljes@vsar.edu.rs>
2023-02-04 10:51:49 +0100

    Styling changes to Details page, displaying image and price, set default font in App.css

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import fetchProduct from "./../../fetchProduct";
+import "./Details.css";
 
 const Details = () => {
   const { id } = useParams();
   const details = fetchProduct(id);
   const { name, price, image } = details[0];
 
-  return <div>{name}</div>;
+  return (
+    <div className="details-container">
+      <div className="details-image">
+        <img src={image}></img>
+      </div>
+      <div className="details-info">
+        <h1>{name}</h1>
+        <div className="price-container">
+          <p className="price">{price}$</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
