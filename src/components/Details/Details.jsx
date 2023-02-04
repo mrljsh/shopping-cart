71ee8abd3db1ac929540a904af7bffcee0010415
Mrljsh <dragan.mrljes@vsar.edu.rs>
2023-02-04 13:31:57 +0100

    Added QuantityInput into details

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import fetchProduct from "./../../fetchProduct";
 import "./Details.css";
+import QuantityInput from "../Cart/QuantityInput";
 
 const Details = () => {
+  const [quantity, setQuantity] = useState(1);
+
   const { id } = useParams();
   const details = fetchProduct(id);
   const { name, price, image } = details[0];
+
+  const handleQuantity = (id, quantity) => {
+    setQuantity(quantity);
+  };
 
   return (
     <div className="details-container">
@@ -17,6 +24,13 @@ const Details = () => {
         <h1>{name}</h1>
         <div className="price-container">
           <p className="price">{price}$</p>
+        </div>
+        <div className="add-to-cart">
+          <QuantityInput
+            handleQuantity={handleQuantity}
+            id={id}
+            quantity={quantity}
+          />
         </div>
       </div>
     </div>
